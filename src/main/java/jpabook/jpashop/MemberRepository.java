5f7e6486bacd63989a889b1bdebf887776890de9
YuHayeon <youha0930@gmail.com>
2022-04-25 21:31:51 +0900

    test: Entity, Repository 동작 테스트
    Member Entity와 Repository를 통해 H2 데이터베이스와
    jpa가 정상적으로 작동되는지 테스트
    p6spy 의존성을 추가해 데이터베이스 value를 확인할 수 있도록 함

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em; // jpa에서 EntityManager 자동으로 생성
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+}
