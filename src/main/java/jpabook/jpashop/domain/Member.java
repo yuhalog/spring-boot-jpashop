e732ed13f800e5a78c7be47514611250d16e760f
YuHayeon <youha0930@gmail.com>
2022-04-27 01:30:33 +0900

    add: Entity 생성

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // 연관관계 주인이 아님을 명시. 여기 값 넣어도 order 테이블 변경X
+    private List<Order> orders = new ArrayList<>();
+
+}
