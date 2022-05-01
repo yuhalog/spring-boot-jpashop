91992ee70d27531df2dfa6453059662abfe9ead2
YuHayeon <youha0930@gmail.com>
2022-05-01 21:40:06 +0900

    feat: Item 엔티티 비즈니스 로직 추가
    NotEnoughStockException은 Item의 재고가 0 미만일 때 예외처리

---
+++ src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.exception;
+
+public class NotEnoughStockException extends RuntimeException {
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+}
