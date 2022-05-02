611cf0d5a77c6f0cf8167b194fe01b22bd05df28
YuHayeon <youha0930@gmail.com>
2022-05-03 01:22:27 +0900

    feat: 기본 생성자 생성을 제한

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -10,6 +12,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -25,7 +28,6 @@ public class OrderItem {
     private Item item;
 
     private int orderPrice; // 주문 가격
-
     private int count; // 주문 수량
 
     //==생성 메서드==//
